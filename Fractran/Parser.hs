cac23eb0080ee1abcf0e21004290fd79039d71a8
Jed Barber <jjbarber@y7mail.com>
2014-02-05 13:25:54 +1100

    Completed most of Fractran parser

--- Fractran/Parser.hs
+++ Fractran/Parser.hs
@@ -9,11 +9,45 @@ import Text.Combinators.Parsec
 
 
 
-data FractranProgram = FractranProgram { fractions :: [(Int,Int)] }
+data FractranProgram = FractranProgram { fractions :: [(Int,Int)]
+                                       , initialValue :: Int }
+    deriving (Show)
 
 
 
 parseFractran :: String -> Either ParseError FractranProgram
 parseFractran = parse fractran "error"
 
+
+
+
+fractran = do
+	f <- many intPair
+	v <- initVal
+	eof
+	return (FractranProgram f v)
+
+
+intPair = do
+	whiteSpace
+	n <- wholeNumber
+	slash
+	d <- wholeNumber
+	return (n,d)
+
+
+slash = char '/'
+
+
+initVal = do
+	whiteSpace
+	v <- wholeNumber
+	whiteSpace
+	return v
+
+
+wholeNumber =
+
+
+whiteSpace = many (oneOf "\t\n\r ")
 
