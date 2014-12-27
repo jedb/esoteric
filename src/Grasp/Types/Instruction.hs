fcdda74a4dbbe515c0aad3f428fda3a46990c848
Jed Barber <jjbarber@y7mail.com>
2014-12-27 20:59:56 +1100

    toInt improved

--- src/Grasp/Types/Instruction.hs
+++ src/Grasp/Types/Instruction.hs
@@ -32,5 +32,10 @@ toFloat :: Instruction -> Maybe Float
 toFloat (Instruction i) = readMaybe i
 
 toInt :: Instruction -> Maybe Int
-toInt (Instruction i) = readMaybe i
+toInt (Instruction i) = do
+	f <- (readMaybe :: String -> Maybe Float) i
+	let g = round f
+	    h = fromIntegral g
+	r <- if (f == h) then Just g else Nothing
+	return r
 
